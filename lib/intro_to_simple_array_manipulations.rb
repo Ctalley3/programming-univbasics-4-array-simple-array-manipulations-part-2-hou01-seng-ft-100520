210209072425a14b1085395d837edf19884935b1
Crystal T <ctalley.cat@gmail.com>
2020-09-23 20:55:55 +0000

    Done.

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,7 +1,30 @@
+
+require './lib/intro_to_simple_array_manipulations.rb'
+
 def using_concat (array1, array2) 
   array1.concat (array2)
  end
 
-def using_insert(array) 
- newelement.insert 
+def using_insert(array)
+ new.insert(array) 
+  new = (4)
  end
+ 
+def using_uniq(array)
+  array.uniq 
+end
+
+def using_flatten(array) 
+  array.flatten
+end
+
+def using_delete(array,string)
+  array.delete = string
+end
+
+def using_delete_at(array,integer)
+  array.delete_at = integer
+end
+
+
+  
